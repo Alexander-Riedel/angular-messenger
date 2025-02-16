2eafacfd6fb1e978250f2678e830dae5d2107380
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-02-16 21:11:48 +0100

    add mat grid list

--- src/app/components/messenger/messenger.component.ts
+++ src/app/components/messenger/messenger.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { SidenavComponent } from "../sidenav/sidenav.component";
-import { MainComponent } from "../main/main.component";
 import { ThreadComponent } from "../thread/thread.component";
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MainComponent } from "../main/main.component";
 
 @Component({
   selector: 'app-messenger',
-  imports: [HeaderComponent, SidenavComponent, MainComponent, ThreadComponent],
+  imports: [HeaderComponent, SidenavComponent, ThreadComponent, MatGridListModule, MainComponent],
   templateUrl: './messenger.component.html',
   styleUrl: './messenger.component.scss'
 })
